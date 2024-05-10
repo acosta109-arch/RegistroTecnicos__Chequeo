4b3cb3e796de8938ee0885983295902bd276b218
camil <camil@LAPTOP-1LI7M39G>
2024-05-09 21:47:02 -0400

    Add project files.

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicosId { get; set; }
+
+    [Required(ErrorMessage = "Favor de ingresar el nombre del tecnico.")]
+    public string Nombres { get; set; }
+    [Required(ErrorMessage = "Favor de ingresar el sueldo por hora del tecnico.")]
+    public decimal SueldoHora { get; set; }
+}
